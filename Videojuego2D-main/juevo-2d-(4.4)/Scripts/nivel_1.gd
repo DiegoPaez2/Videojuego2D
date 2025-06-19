a14a889a6e5c390ab9bbe08a479bcb4270874377
vanessazurita <vnzurita@espe.edu.ec>
2025-06-18 20:18:43 -0500

    Revertir

--- Videojuego2D-main/juevo-2d-(4.4)/Scripts/nivel_1.gd
+++ Videojuego2D-main/juevo-2d-(4.4)/Scripts/nivel_1.gd
@@ -1,19 +1,8 @@
 extends Node2D
-
 @onready var jugador = $Jugador
 var posicion_inicial = Vector2.ZERO
-
 func _ready():
 	posicion_inicial = jugador.position  # Guarda la posición inicial al iniciar el nivel
-
-# Aquí escuchamos las teclas presionadas
-func _input(event):
-	if event.is_action_pressed("pausar"):
-		_on_pausar_pressed()
-	elif event.is_action_pressed("continuar"):
-		_on_continuar_pressed()
-	elif event.is_action_pressed("reiniciar"):
-		_on_reiniciar_pressed()
 
 func _on_pausar_pressed() -> void:
 	get_tree().paused = true
